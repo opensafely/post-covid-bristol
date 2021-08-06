24b92641b71ef6cc0af4fc23226b87da4282e376
Jon Massey <jon.massey@thedatalab.org>
2021-08-06 14:39:22 +0000

    region

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -212,5 +212,24 @@ study = StudyDefinition(
             },
         },
     ),
-
+    
+    cov_region=patients.registered_practice_as_of(
+        "index_date",
+        returning="nuts1_region_name",
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "North East": 0.1,
+                    "North West": 0.1,
+                    "Yorkshire and the Humber": 0.1,
+                    "East Midlands": 0.1,
+                    "West Midlands": 0.1,
+                    "East of England": 0.1,
+                    "London": 0.2,
+                    "South East": 0.2,
+                },
+            },
+        },
+    )
 )
