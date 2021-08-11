34e45940b2a1e7734dc3aef83f5347d1effc0744
Alex Walker <alex.walker@thedatalab.org>
2021-08-11 17:56:51 +0100

    dmd snomed fix

--- analysis/variable_loop.py
+++ analysis/variable_loop.py
@@ -3,19 +3,30 @@ import inspect
 import random
 
 
-def get_variable_definition(codelist):
+def get_variable_definition(codelist, name):
     """
-    Derives the variable definition depending on the codelist.system
-    The variables returned are simple binary variables before the index date.
+    Derives the variable definition depending on the codelist.system (and in the case
+    of snomed, also from the codelist name). The variables returned are simple binary
+    variables before the index date.
     """
     if codelist.system == "ctv3":
         return patients.with_these_clinical_events(codelist, on_or_before="index_date")
+    if codelist.system == "snomed":
+        if name[-8:-4] == "_dmd":
+            return patients.with_these_medications(codelist, on_or_before="index_date")
+        if name[-13:-4] == "_clinical":
+            return patients.with_these_clinical_events(
+                codelist, on_or_before="index_date"
+            )
+        raise ValueError(
+            """'snomed' type codelists must be named with the either the suffix '_dmd'
+            or '_clinical' to distinguish between medicine and clinical type codelists
+            respectively"""
+        )
     if codelist.system == "icd10":
         return patients.admitted_to_hospital(
             with_these_diagnoses=codelist, on_or_before="index_date"
         )
-    if codelist.system == "snomed":
-        return patients.with_these_medications(codelist, on_or_before="index_date")
 
 
 def retrieve_name(var):
@@ -42,12 +53,13 @@ def get_codelist_variable(codelists):
     - a single variable if len(codelists) == 1
     - a combined variable (using OR)
     """
+    string_list = [retrieve_name(v) for v in codelists]
     if len(codelists) > 1:
-        string_list = [retrieve_name(v) for v in codelists]
         logic = " OR ".join(string_list)
         sub_var_dict = {
-            f"{s}": get_variable_definition(c) for s, c in zip(string_list, codelists)
+            f"{s}": get_variable_definition(c, s)
+            for s, c in zip(string_list, codelists)
         }
         return patients.satisfying(logic, **sub_var_dict)
-    else:
-        return get_variable_definition(codelists[0])
+
+    return get_variable_definition(codelists[0], string_list[0])
