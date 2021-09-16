e2d3bcca44930ae94b95be2e1ffd2c07a08bd143
Jon Massey <jon.massey@thedatalab.org>
2021-09-16 14:53:50 +0100

    Merge pull request #8 from opensafely/add_noncovar_variables
    Add exposure and outcome variables

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,6 +1,30 @@
 from os import system
 from cohortextractor import codelist, codelist_from_csv
 
+covid_codes = codelist_from_csv(
+    "codelists/opensafely-covid-identification.csv",
+    system="icd10",
+    column="icd10_code",
+)
+
+covid_primary_care_positive_test = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+covid_primary_care_code = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+covid_primary_care_sequalae = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
 opensafely_ethnicity_codes_16 = codelist_from_csv(
     "codelists/opensafely-ethnicity.csv",
     system="ctv3",
